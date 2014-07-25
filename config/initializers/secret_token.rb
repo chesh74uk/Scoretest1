e33e73041374c762762958870739876764d03889
Chesh <chesh@chesh.info>
2014-07-25 16:51:31 +0100

    fixed secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scoretest1::Application.config.secret_key_base = 'e1267074b399b741f8f713130551b30e50c797c75f4c836e400fa7e99ce90fbcd7bae5152ee5e1990f4abbb7e095b47eeec8345f731041aca3cffa6a4e3618d1'
