dab81f362ea9949e1666b4d9e5b7c81227b7b5c1
Chesh <chesh@chesh.info>
2014-07-29 19:48:09 +0100

    fixed new page

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,10 +1,19 @@
 module RacesHelper
 
-  def total_score
+  def total_score(team)
 
+  if team == "home"
     blue_sum = @races.sum(:blue_score)
     red_sum = @races.sum(:red_score)
-    total_home = blue_sum + red_sum
+    @total = blue_sum + red_sum
+  else
+    white_sum = @races.sum(:white_score)
+    yellow_sum = @races.sum(:yellow_score)
+    @total = white_sum + yellow_sum
+
+  end
+
+
 
 
     end
